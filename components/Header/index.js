4855b4885f2ff9648b760d20535c9e53831f8435
Bryan Szendel <bryan.szendel@gmail.com>
2019-07-12 09:35:02 -0600

    header component has been created in the DOM

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,10 +3,33 @@
 // Using a function create the component you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+let headerContainer = document.querySelector('.header-container')
+let newHeader = Header()
+headerContainer.appendChild(newHeader)
+
+function Header() {
+    let header = document.createElement('header')
+    let dateSpan = document.createElement('span')
+    let title = document.createElement('h1')
+    let temp = document.createElement('span')
+
+    header.appendChild(dateSpan)
+    header.appendChild(title)
+    header.appendChild(temp)
+    
+    header.classList.add('header')
+    dateSpan.classList.add('date')
+    temp.classList.add('temp')
+
+    dateSpan.textContent = 'MARCH 28, 2019'
+    title.textContent = 'Lambda Times'
+    temp.textContent = '98°'
+
+    return header
+}
